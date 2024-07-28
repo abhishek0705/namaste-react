c73528799bbaf3fd0eeca7eab670acc4422c4440
Abhishek Natani <abhisheknatani@mplgaming.com>
2024-07-28 12:14:05 +0530

    ep-2

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
-const parent = React.createElement(
-    "div",
-    {id:'parent'},
-    [
-        React.createElement(
-        "div", 
-        {id:'child'}, 
-        [
-            React.createElement("h1", {}, "I am h1 tag!"),
-            React.createElement("h2", {}, "I am h2 tag!")
-        ]),
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-        React.createElement(
-            "div", 
-            {id:'child2'}, 
-            [
-                React.createElement("h1", {}, "I am h1 tag!"),
-                React.createElement("h2", {}, "I am h2 tag!")
-            ])
-    ]);
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const parent = React.createElement(
+  "div", //tag
+  { id: "parent" }, // attribute
+  [
+    // childred
+    React.createElement("div", { id: "child", key: "child" }, [
+      React.createElement("h1", { key: "h1" }, "I am h1 tag!"),
+      React.createElement("h2", { key: "h2" }, "I am h2 tag!"),
+    ]),
+
+    React.createElement("div", { id: "child2", key: "child2" }, [
+      React.createElement("h1", { key: "h3" }, "I am h1 tag!"),
+      React.createElement("h2", { key: "h4" }, "I am h2 tag!"),
+    ]),
+  ]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Namaste React
+# Learn React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster builds
+- Image Optimization
+- Minification
+- Building
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - removes unused code
+- Different dev and prod bundles
